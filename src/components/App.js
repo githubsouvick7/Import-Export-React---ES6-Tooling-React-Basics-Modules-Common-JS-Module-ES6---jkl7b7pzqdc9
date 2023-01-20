db60e3a59ad13219db1d86cde6194b0a5a04b9a0
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-01-20 17:08:06 +0530

    Update App.js
    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from './Heading';
+import InputQuary from "./InputQuery"
+import SubHeading from './SubHeading'
+import SubmitButton from './SubmitButton'
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuary />
+      <SubHeading />
+      <SubmitButton />
     </div>
   )
 }
+
+export default App;
+
 
 
